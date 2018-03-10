624b1c50f578feab2e191e6b5c6e86e3979c5d92
GatomanJuarez <juarez_devora_ja@hotmail.com>
2018-03-09 18:21:10 -0600

    Signup

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { CalculatorPage } from '../calculator/calculator';
 import { UserPage } from '../user/user';
+import { LoginPage } from '../login/login';
 import { AuthSevice } from '../../services/auth/auth';
 
 @Component({
@@ -24,6 +25,6 @@ export class HomePage {
     }
 
     goOut(){
-        window.close();
+        this.navCtrl.push(LoginPage);
     }
 }
--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
+import {Http, Response} from '@angular/http';
+import 'rxjs/Rx';
 
 /**
  * Generated class for the SignupPage page.
@@ -14,15 +17,50 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'signup.html',
 })
 export class SignupPage {
+  email: string;
+  contrasena: string;
+  contrasena2: string;
+  nombre: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  resultado: any;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,  private toastCtrl:ToastController, public http:Http) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SignupPage');
   }
 
-  menssage(){
-    console.log("Hoa");
+  registrar(){
+    if(this.email != null && this.nombre != null && this.contrasena != null && this.contrasena2 != null){
+        this.http.post('https://apex.oracle.com/pls/apex/indeme/INcreate/', {
+          'correo': this.email,
+          'nombres': this.nombre,
+          'contrasena': this.contrasena
+        }).map((response:Response)=>{
+          return response.json();
+        }).subscribe(
+          ()=> {console.log("Success");
+          this.presentToast('Usuario registrado correctamente.');
+        },
+          (error)=>{
+            console.log('error');
+            this.presentToast('Error al registrarse porfavor intentelo mas tarde.');
+          }
+        )
+  }
+    else{
+    this.presentToast('Rellena todos los campos de manera correcta.');
+  }
+}
+
+
+  presentToast(message) {
+    let toast = this.toastCtrl.create({
+      message: message + "" ,
+      duration: 3000,
+      position: 'middle'
+    });
+    toast.present();
   }
 }
