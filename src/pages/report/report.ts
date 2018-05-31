db856340257c657ccaa20b901b92b903ac0d0cbd
GatomanJuarez <juarez_devora_ja@hotmail.com>
2018-05-31 09:02:27 -0500

    Frontend reporte

--- src/pages/report/report.ts
+++ src/pages/report/report.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import {
+  ToastController
+} from 'ionic-angular';
+import {Http} from '@angular/http';
+import 'rxjs/Rx';
 /**
  * Generated class for the ReportPage page.
  *
@@ -19,43 +23,71 @@ export class ReportPage {
   answer: any;
   askSliderCount: any;
   title: any;
+  for: any;
+  by: any;
   graphic: any [] = [];
   annexA: any [] = [];
   annexB: any [] = [];
   annexC: any [] = [];
   annexD: any [] = [];
-  answerannexA: any [] = [];
-  answerannexB: any [] = [];
-  answerannexC: any [] = [];
-  answerannexD: any [] = [];
+  answerannexA: any;
+  answerannexB: any;
+  answerannexC: any;
+  answerannexD: any;
   operacion: any [] = [];
   variable: any;
   dimension: any;
   indicator: any;
   item: any;
+  problem: any;
+  object: any;
+  research: any;
+  information: any;
+  instrumentation: any;
+  population: any;
+  analysisG: any;
+  analysisC: any;
+  conclusion: any;
+  graphicc: any;
+  showVariable: any [] = [];
+  resultado: any;
+  tamano: number;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private toastCtrl: ToastController,  public http:Http) {
     this.encuestaId = navParams.get('encuesta_id');
     console.log(this.encuestaId);
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ReportPage');
+    this.http.get('https://apex.oracle.com/pls/apex/indeme/INpollsSearch/' + this.encuestaId).map(res => res.json()).subscribe(data => {
+      this.resultado = data.items;
+      console.log(this.resultado.length);
+      this.tamano = this.resultado.length;
+    });
+  }
+
+  doReport(){
+    if(this.title != null && this.for != null && this.by != null && this.title != null && this.problem != null &&
+      this.object != null && this.research != null && this.information != null && this.instrumentation != null &&
+      this.population != null && this.analysisC != null && this.analysisG != null && this.conclusion != null
+      && this.graphicc != null){
+        this.presentToast("Hola");
+      }
+    else{
+      this.presentToast("Rellena los campos solicitados.")
+    }
+
   }
 
   addAskOption(){
     if(this.variable != null && this.dimension != null && this.indicator != null && this.item != null){
+    this.showVariable.push(this.variable);
     this.operacion.push(this.variable);
     this.operacion.push(this.dimension);
     this.operacion.push(this.indicator);
     this.operacion.push(this.item);
     }
-  }
-
-  deleteAskOption(){
-    if(this.asks.length >= 1){
-      this.asks.pop();
-    } 
   }
 
   addGraphic(){
@@ -72,44 +104,24 @@ export class ReportPage {
     this.annexA.push(this.answerannexA);
   }
 
-  deleteA(){
-    if(this.annexA.length >= 1){
-      this.annexA.pop();
-    } 
-  }
-
   addB(){
     this.annexB.push(this.answerannexB);
-  }
-
-  deleteB(){
-    if(this.annexB.length >= 1){
-      this.annexB.pop();
-    } 
   }
 
   addC(){
     this.annexC.push(this.answerannexC);
   }
 
-  deleteC(){
-    if(this.annexC.length >= 1){
-      this.annexC.pop();
-    } 
-  }
-
   addD(){
     this.annexD.push(this.answerannexD);
   }
-
-  deleteD(){
-    if(this.annexD.length >= 1){
-      this.annexD.pop();
-    } 
-  }
-
-  doReport(){
-
-  }
   
+  presentToast(message) {
+    let toast = this.toastCtrl.create({
+      message: '' + message,
+      duration: 3000,
+      position: 'middle'
+    });
+    toast.present();
+  }
 }
