413d1dfe9c61796479092ef59385d126ffa9be23
GatomanJuarez <juarez_devora_ja@hotmail.com>
2018-06-13 12:34:33 -0500

    Ventana de resultados

--- src/pages/resultpolls/resultpolls.ts
+++ src/pages/resultpolls/resultpolls.ts
@@ -26,34 +26,9 @@ export class ResultpollsPage {
       content: 'Cargando resultados...'
   });
   this.loading.present();
-    console.log('ionViewDidLoad ResultpollsPage');
-    console.log("000000000000000");
-    console.log(this.encuestaId);
-    console.log("000000000000000");
-    this.http.get('https://apex.oracle.com/pls/apex/indeme/INaskResult/' + this.encuestaId).map(res => res.json()).subscribe(data => {
+    this.http.get('https://apex.oracle.com/pls/apex/indeme/IN/askAll/' + this.encuestaId).map(res => res.json()).subscribe(data => {
       this.resultado2 = data.items;
-      console.log(this.resultado2);
       this.loading.dismiss();
-      for(var i = 0; i <this.resultado2.length; i++){
-        this.pregunta.push(this.resultado2[i].pregunta);
-        this.result.push(this.resultado2[i].resultado);
-      }
-
-      for(var a = 0; a <this.pregunta.length; a++){
-        for(var b = 0; b <this.pregunta.length; b++){
-          if(this.pregunta[a] == this.pregunta[b]){
-            this.resultados.push(1);
-          }
-        }
-      }
-
-      for(var e = 0; e <this.resultados.length; e++){
-      console.log(this.resultados[e]);
-      }
     });
-
-    
   }
-
-
 }
