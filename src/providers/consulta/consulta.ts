df26d7f69df3d3387ffa26f9d4e0f17f5a8190e3
GatomanJuarez <juarez_devora_ja@hotmail.com>
2018-06-15 10:27:13 -0500

    Buscadores

--- src/providers/consulta/consulta.ts
+++ src/providers/consulta/consulta.ts
@@ -1,11 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
-/*
-  Generated class for the ConsultaProvider provider.
 
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class ConsultaProvider {
 
@@ -26,7 +21,24 @@ export class ConsultaProvider {
     return new Promise((resolve, reject) => {
       this.http.get('https://apex.oracle.com/pls/apex/indeme/IN/SearchLibrary/' + nombre).map(res => res.json())
         .subscribe(data => {
-          console.log(data.items);
+          resolve(data.items);
+        });
+    });
+  }
+
+  public getListResult(encuestaId, nombre){
+    return new Promise((resolve, reject) => {
+      this.http.get('https://apex.oracle.com/pls/apex/indeme/IN/searchResult/' + encuestaId + "/" + nombre).map(res => res.json())
+        .subscribe(data => {
+          resolve(data.items);
+        });
+    });
+  }
+
+  public getListSeeAsk(encuestaId, nombre){
+    return new Promise((resolve, reject) => {
+      this.http.get('https://apex.oracle.com/pls/apex/indeme/IN/searchPollAsk/' + encuestaId + "/" + nombre).map(res => res.json())
+        .subscribe(data => {
           resolve(data.items);
         });
     });
